e75508325797e8b64cbe9598e664f1c54fc4ec72
Osmez <osmez9tech@gmail.com>
2025-04-08 03:44:47 -0700

    remove rights code

--- src/app/ui/footer.jsx
+++ src/app/ui/footer.jsx
@@ -22,14 +22,14 @@ export default function RandFooter({lng}){
             <>
                 <h2 style={{fontWeight:900}} className={`w-full text-center text-[50px] ${ub.className}`}>Rand <br/> Stationery</h2>
                 <h2 className={`text-center ${ub.className} bg-stone-900 rounded-md p-2`}>Created By Osama Namur</h2>
-                <h2 className={`text-center ${ub.className} bg-stone-900 rounded-md p-2`}>Copy Rights &#169; Osama Namur</h2>
+                <h2 className={`text-center ${ub.className} bg-stone-900 rounded-md p-2`}>Copy Rights Osama Namur</h2>
 
             </>
             :
             <>
                 <h2 style={{fontWeight:500}} className={`w-full text-center  text-[100px] ${lali.className}`}>مكتبة  رند </h2>
                 <h2 className={`text-center bg-stone-900 ${mad.className} p-2 rounded-md`}>أنشئ بواسطة أسامة نمور</h2>
-                <h2 className={`text-center bg-stone-900 ${mad.className} p-2 rounded-md`}>جميع الحقوق محفوظة &#169; أسامة نمور</h2>
+                <h2 className={`text-center bg-stone-900 ${mad.className} p-2 rounded-md`}>جميع الحقوق محفوظة  أسامة نمور</h2>
 
             </>
             
