b343fcc2decdcf930b063207ec22cd8849b685ce
Osmez <osmez9tech@gmail.com>
2025-04-18 21:21:13 -0700

    add test response

--- src/app/actions/auth.jsx
+++ src/app/actions/auth.jsx
@@ -100,3 +100,51 @@ export async function SendCode(state, formData){
   }
  
 }
+
+
+
+export async function testAuth(state, formData ) {
+
+  try {
+    const uname = formData.get('email');
+    const upassword = formData.get('password');
+/*
+    const validate = SignupFormSchema.safeParse({
+      name: 'osama',
+      email: uname,
+      password: upassword,
+    })
+
+    if(!validate.success){
+      //console.log(validate.error.flatten().fieldErrors.)
+      return validate.error.flatten().fieldErrors.email + '' + validate.error.flatten().fieldErrors.password
+      
+    }
+*/
+  
+    const hdrs = new Headers();
+    hdrs.append('Content-Type' ,  'application/json');
+    hdrs.append('Accept' ,  'application/json');
+    const body={email: uname, password: upassword}
+    
+    const res = await fetch('http://osmezbase.infy.uk/public/api/login',{
+      method:"POST",
+      headers: hdrs,
+      body:JSON.stringify(body)
+    });
+
+    const jres = await res.text();
+       
+    return jres;
+    
+  } catch (error) {
+    if (error) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {message:'invalid'}
+        default:
+          return {message:error.message};
+      }
+    }  
+  }
+}
